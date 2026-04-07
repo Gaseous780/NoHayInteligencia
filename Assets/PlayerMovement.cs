60a4189247ea5d68fda91f89457fed8e15e6fa0c
astridt3 <rocio.beck05@gmail.com>
2026-04-06 22:06:39 -0300

    rotacion

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,23 +5,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public InputAction moveAction;
     public float speed = 5f;
-    public float speedRotation = 3f;
-    public Rigidbody rb;
+    public float speedRotation = 10f;
 
     private void Awake()
     {
-        moveAction.Enable(); //activa input
+        moveAction.Enable();
     }
+
     void RotatePlayer(Vector3 direction)
     {
-        transform.forward = Vector3.Lerp(transform.forward, direction, speedRotation * Time.deltaTime);
-        rb.linearVelocity = direction * speed;
+        if (direction == Vector3.zero) return;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetAngle,speedRotation * 500f * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
     }
+
     private void Update()
     {
-        Vector2 moveInput = moveAction.ReadValue<Vector2>(); //lee el input del movimiento segn el vector 2 (up,down,right,left)
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y); //lo pasa a vector 3 para que este en xyz 
-        transform.position += move * speed* Time.deltaTime; //mueve pos
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        transform.position += move.normalized * speed * Time.deltaTime;
         if (move != Vector3.zero)
         {
             RotatePlayer(move);
